e1dba7c76edc036b0c4fa4f72a80b7276e718910
Hamilkar247 <adonnibal96@gmail.com>
2020-11-20 01:17:24 +0100

    ISS-24 app serilizer and viewsets for classes core

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,14 @@
 from django.urls import path, include
 from . import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'user', views.UserViewSet)
+router.register(r'deck', views.DeckViewSet)
+router.register(r'flashCard', views.FlashCardViewSet)
+router.register(r'deckFlashCard', views.DeckFlashCardViewSet)
+router.register(r'level', views.LevelViewSet)
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
