f0f330e0dad09650b9d52ed05d0af00bc7560540
Jakub Latusek <jk.latusek@gmail.com>
2020-12-03 20:04:39 +0100

    [ISS-41] Add factories for models in project
    Signed-off-by: Jakub Latusek <jk.latusek@gmail.com>

---
+++ core/factories.py
@@ -0,0 +1,48 @@
+import factory
+from . import models
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    first_name = factory.Faker('first_name')
+    last_name = factory.Faker('last_name')
+    admin = False
+
+
+class DeckFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Deck
+
+    name = factory.Sequence(lambda n: 'deck%s' % n)
+    description = factory.Sequence(lambda n: 'description%s' % n)
+    tag = factory.Sequence(lambda n: 'tag%s' % n)
+    account = factory.SubFactory(UserFactory)
+    owner = factory.SubFactory(UserFactory)
+
+class FlashCardFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.FlashCard
+
+    name = factory.Sequence(lambda n: 'flashCard%s' % n)
+    reverse = factory.Sequence(lambda n: 'reverse%s' % n)
+    averse = factory.Sequence(lambda n: 'averse%s' % n)
+    tip = factory.Sequence(lambda n: 'tip%s' % n)
+
+class LevelFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.Level
+
+    name = factory.Sequence(lambda n: 'level%s' % n)
+    repeat_frequency = factory.fuzzy.FuzzyInteger(0, 1000)
+
+class DeckFlashCardFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = models.DeckFlashCard
+
+    deck = factory.SubFactory(DeckFactory)
+    flashCard = factory.SubFactory(FlashCardFactory)
+    level = factory.SubFactory(LevelFactory)
+    good_answers = factory.fuzzy.FuzzyInteger(0, 1000)
+    bad_answers = factory.fuzzy.FuzzyInteger(0, 1000)
+    
