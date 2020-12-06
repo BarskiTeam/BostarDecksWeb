3b0c8b83c2fc58022f937a602c4c02d9c57cb075
Hamilkar247 <adonnibal96@gmail.com>
2020-12-06 14:39:37 +0100

    [ISS-42] use api to show list of decks
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

---
+++ front/static/front/js/decks.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+  console.log("decks.js")
+  $.ajax({
+      url: "http://127.0.0.1:8000/api/v1/deck/",
+      type: "GET",
+  }).done(function (response) {
+      for(i=0; i<response.length; i++){
+          console.log("response[i].id:"+response[i].id)
+          $('#table_decks').append(
+            '<tr>' +
+                '<td>' + response[i].id + '</td>' +
+                '<td>' + response[i].name + '</td>' +
+                '<td>' + response[i].description + '</td>' +
+                '<td>' + response[i].public + '</td>' +
+                '<td>' + ' <a href="/front/decks/' + response[i].id + '" class="button">' + response[i].name + '</a>' +
+                '</td>' +
+             '</td>');
+          }
+  }).fail(function () {
+    alert("Exist problem with connection to api")
+  })
+})
