ff712130b99c8d7210b0a6c6a135cdcd896a4951
Hamilkar247 <adonnibal96@gmail.com>
2020-12-09 07:40:50 +0100

    [ISS-61] data_menu.js - func to take number_of_deck to check
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- front/static/front/js/deck_menu.js
+++ front/static/front/js/deck_menu.js
@@ -1,8 +1,25 @@
 url_home="http://127.0.0.1:8000"
+let number_of_deck=null
+let regex = null
+function func_for_number_of_deck(){
+  let url = window.location.href
+  table_of_url = url.split('/')
+  console.log(table_of_url)
+  if (table_of_url[table_of_url.length - 1] == '') {
+    number_of_deck = table_of_url[table_of_url.length - 2]
+  }
+  else {
+    number_of_deck = table_of_url[table_of_url - 1]
+  }
+  console.log("url:"+window.location.href)
+  console.log("number_of_deck:"+number_of_deck)
+}
+
 $( document ).ready(function() {
+  func_for_number_of_deck()
   console.log("deck_menu.js")
   $.ajax({
-      url: url_home+get_urls().deck_flashcards,
+      url: url_home + get_urls().api_deck + number_of_deck+ "/flashCards/",
       type: "GET",
   }).done(function (response) {
       for(i=0; i<response.length; i++){
