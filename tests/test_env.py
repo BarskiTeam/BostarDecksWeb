e5a937a5e1d9c9d4917c7b45a7473da34ae4253e
Hamilkar247 <adonnibal96@gmail.com>
2020-12-20 21:05:36 +0100

    [ISS-118] create .env for test on github server
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- tests/test_env.py
+++ tests/test_env.py
@@ -28,32 +28,25 @@ def env_example_data():
 def test_variable_name_in_env_is_uppercase(env_data):
     """check used name of variable are uppercase"""
     for variable in env_data:
-        if variable.upper() == variable:
-            continue
-        else:
-            assert False
-    assert True
+        assert variable.upper() == variable, f"{variable} isn't uppercase"
 
 
 def test_variable_name_in_env_example_is_uppercase(env_example_data):
     """check used name of variable are uppercase"""
     for variable in env_example_data:
-        if variable.upper() == variable:
-            continue
-        else:
-            assert False
-    assert True
+        assert variable.upper() == variable, f"{variable} isn't uppercase"
 
 
-def test_compare_variable_name_and_order_of_env_and_env_example_files(env_data, env_example_data):
+def test_compare_variable_name_of_env_and_env_example_files(env_data, env_example_data):
     """
     check both of file - .env.example and .env have that same name of variables
-    and that same order of this variables
-    Attention - i remove whitespaces on begin and end
     """
-    str_env_data = ' '.join(env_data).strip()
-    str_env_example_data = ' '.join(env_example_data).strip()
-    if str_env_data == str_env_example_data:
-        assert True
-    else:
-        assert False
+    env_data.sort()
+    env_example_data.sort()
+    for variable in env_data:
+        assert variable in env_example_data, f"{variable} is not exist in .env.example file"
+        assert env_data.count(variable) == 1, f"{variable} is duplicated in .env"
+
+    for variable in env_example_data:
+        assert variable in env_data, f"{variable} is not exist in .env file"
+        assert env_example_data.count(variable) == 1, f"{variable} is duplicated in .env.example"
