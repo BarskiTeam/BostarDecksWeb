b7f6f81fc1c46254c30be6d5b5a649b143e39f66
Hamilkar247 <adonnibal96@gmail.com>
2021-01-02 00:20:36 +0100

    [ISS-133] black code ordung - bostardecksweb - root folder
    Signed-off-by: Hamilkar247 <adonnibal96@gmail.com>

--- bostardecksweb/asgi.py
+++ bostardecksweb/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bostardecksweb.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bostardecksweb.settings")
 
 application = get_asgi_application()
